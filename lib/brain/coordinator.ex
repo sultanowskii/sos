d07cf9346f8fa5e9d97a8834227b9eaed7bccb39
sultanowskii <dsr431@yandex.ru>
2025-01-19 15:40:14 +0300

    add put/delete/get operation skeletons to coordinator

--- lib/brain/coordinator.ex
+++ lib/brain/coordinator.ex
@@ -11,38 +11,70 @@ defmodule Brain.Coordinator do
   end
 
   def init(state) do
-    schedule_workers_health_check()
+    schedule_agents_health_check()
     {:ok, state}
+  end
+
+  def handle_call({:put_object, bucket, key, data}, _from, _state) do
+    agent = pick_random_agent()
+
+    GenServer.call({:global, agent}, {:put_object, bucket, key, data})
+  end
+
+  def handle_call({:get_object, bucket, key}, _from, _state) do
+    agent = nil
+    # TODO: take from DB by (bucket, key)
+
+    GenServer.call({:global, agent}, {:get_object, bucket, key})
+  end
+
+  def handle_call({:delete_object, bucket, key}, _from, _state) do
+    agent = nil
+    # TODO: take from DB by (bucket, key)
+
+    GenServer.call({:global, agent}, {:delete_object, bucket, key})
   end
 
   # A typical way to set up a periodic work:
   # handle_info/2 + Process.send_after
-  def handle_info(:check_workers_health, state) do
-    case registered_storage_agents() do
+  def handle_info(:check_agents_health, state) do
+    case alive_storage_agents() do
       [] ->
         IO.puts("No worker is online")
 
-      workers = [_ | _] ->
-        for worker <- workers do
-          result = GenServer.call({:global, worker}, :health_check)
+      agents = [_ | _] ->
+        for agent <- agents do
+          result = GenServer.call({:global, agent}, :health_check)
 
-          {:storage_agent, worker_name} = worker
+          {:storage_agent, agent_name} = agent
 
           case result do
             :ok ->
-              IO.puts("#{worker_name} is alive")
+              IO.puts("#{agent_name} is alive")
 
             _ ->
-              IO.puts("#{worker_name} isn't alive")
+              IO.puts("#{agent_name} isn't alive")
           end
         end
     end
 
-    schedule_workers_health_check()
+    schedule_agents_health_check()
     {:noreply, state}
   end
 
-  defp registered_storage_agents do
+  defp pick_random_agent do
+    agents = alive_storage_agents()
+
+    index =
+      agents
+      |> Enum.count()
+      |> :rand.uniform()
+      |> Kernel.-(1)
+
+    Enum.at(agents, index)
+  end
+
+  defp alive_storage_agents do
     :global.registered_names()
     |> Enum.filter(fn name ->
       case name do
@@ -52,7 +84,7 @@ defmodule Brain.Coordinator do
     end)
   end
 
-  defp schedule_workers_health_check do
-    Process.send_after(self(), :check_workers_health, @health_check_interval)
+  defp schedule_agents_health_check do
+    Process.send_after(self(), :check_agents_health, @health_check_interval)
   end
 end
