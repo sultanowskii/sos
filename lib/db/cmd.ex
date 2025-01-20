953363153616f0a75f1c6adb53ed0dc82dce4fb5
jaba <368601@edu.itmo.ru>
2025-01-20 05:52:03 +0300

    specified directory for storing data of db

--- lib/db/cmd.ex
+++ lib/db/cmd.ex
@@ -1,15 +1,37 @@
 defmodule Db.Cmd do
+  @moduledoc """
+   Module for initializing and configuring mnesia
+  """
+
   alias :mnesia, as: Mnesia
 
+  @db_store "#{System.user_home()}/.mnesia"
+
   def init do
+    # Сначала устанавливаем директорию для хранения данных
+    set_disk_location()
+
+    # Создаем схему и запускаем Mnesia
     Mnesia.create_schema([node()])
-    init_tables()
     Mnesia.start()
+
+    # Инициализируем таблицы
+    init_tables()
   end
 
-  defp init_tables() do
+  defp init_tables do
     Db.Storage.init()
     Db.Bucket.init()
     Db.Object.init()
+  end
+
+  defp set_disk_location do
+    disk_dir = @db_store
+
+    # Создаем директорию, если она не существует
+    File.mkdir_p!(disk_dir)
+
+    # Устанавливаем путь для хранения базы данных
+    Mnesia.change_config(:dir, disk_dir)
   end
 end
